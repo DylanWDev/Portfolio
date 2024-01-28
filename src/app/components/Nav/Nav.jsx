1315dbc1e6e34805fbf356d5015fdc97c75e0e2d
w3schools-test <test@w3schools.com>
2024-01-28 14:30:59 -0500

    added isActive to my nav

--- src/app/components/Nav/Nav.jsx
+++ src/app/components/Nav/Nav.jsx
@@ -1,10 +1,19 @@
 "use client";
-import React from "react";
+import { useEffect, useState } from "react";
 import Link from "next/link";
 import { Popover } from "@headlessui/react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+import { useRouter } from "next/navigation";
 
 const Nav = () => {
+  const [currentPath, setCurrentPath] = useState("");
+
+  useEffect(() => {
+    setCurrentPath(window.location.pathname);
+  }, []);
+
+  const isCurrentPage = (href) => currentPath === href;
+
   return (
     <Popover className="nav container text-white mx-auto flex items-center px-6 py-2 h-20">
       <Link className=" font-bold text-lg" href="/">
@@ -12,12 +21,49 @@ const Nav = () => {
       </Link>
       <div className="grow">
         <div className="hidden text-lg sm:flex items-center justify-end gap-5">
-          <Link className="relative w-fit transition ease-in-out hover:text-violet-600 block after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center" href="about">
-            About
+          <Link href="/about">
+            <span
+              className={`relative w-fit block transition ease-in-out hover:text-violet-600 cursor-pointer after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${
+                isCurrentPage("/about") ? "text-violet-600" : ""
+              }`}
+            >
+              About
+              <span
+                className={`absolute block after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${
+                  isCurrentPage("/about") ? "scale-x-100" : ""
+                }`}
+              ></span>
+            </span>
           </Link>
-          <Link className="relative w-fit block transition ease-in-out hover:text-violet-600 after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center" href="blog">Blog</Link>
-          <Link className="relative w-fit block transition ease-in-out hover:text-violet-600 after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center" href="projects">Work</Link>
-          <button className="hire-me flex  rounded-lg transition hover:bg-violet-600 text-white font-semibold py-2 px-4 border border-gray-400  shadow">
+          <Link href="/blog">
+            <span
+              className={`relative w-fit block transition ease-in-out hover:text-violet-600 cursor-pointer after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${
+                isCurrentPage("/blog") ? "text-violet-600" : ""
+              }`}
+            >
+              Blog
+              <span
+                className={`absolute block after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${
+                  isCurrentPage("/blog") ? "scale-x-100" : ""
+                }`}
+              ></span>
+            </span>
+          </Link>
+          <Link href="/work">
+            <span
+              className={`relative w-fit block transition ease-in-out hover:text-violet-600 cursor-pointer after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${
+                isCurrentPage("/work") ? "text-violet-600" : ""
+              }`}
+            >
+              Work
+              <span
+                className={`absolute block after:block after:content-[''] after:absolute after:h-[3px] after:bg-violet-600 after:w-full after:scale-x-0 after:hover:scale-x-100 after:transition after:duration-300 after:origin-center ${
+                  isCurrentPage("/work") ? "scale-x-100" : ""
+                }`}
+              ></span>
+            </span>
+          </Link>
+          <button className="hire-me flex rounded-lg transition hover:bg-violet-600 text-white font-semibold py-2 px-4 border border-gray-400  shadow">
             Hire Me
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -43,7 +89,8 @@ const Nav = () => {
 
       <Popover.Panel
         focus
-        className="z-5 absolute right-0 left-auto top-0 origin-top-right transform transition md:hidden"      >
+        className="z-5 absolute right-0 left-auto top-0 origin-top-right transform transition md:hidden"
+      >
         <div className=" rounded-lg p-5 bg-black shadow-lg ring-1 ring-black ring-opacity-5 divide-y-2 divide-gray-50">
           <div clas4sName="px-5 pt-5 pb-6">
             <div className="flex items-center justify-between">
@@ -71,7 +118,7 @@ const Nav = () => {
                 </Link>
                 <Link
                   className="focus:outline-none focus:ring-2 focus:ring-insert focus:ring-gray-500 px-2"
-                  href="/projects"
+                  href="/work"
                 >
                   Work
                 </Link>
