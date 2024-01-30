064f36a4f0fa0dc7a2ee3ea0c12fc746d0568960
Dylan Wyrick <xpoweroutage@gmail.com>
2024-01-30 16:24:41 -0500

    added about component and started on one page scroller

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,18 @@
-"use client"
+"use client";
 import Stars from "./components/Stars/Stars.jsx";
 import Nav from "./components/Nav/Nav.jsx";
-import React, { useEffect } from 'react';
+import React from 'react';
+import About from "./components/About/About.jsx";
 
 export default function Home() {
   
-
   return (
-    <div className="">
-      <div className="absolute top-0 w-full z-50">
-        <Nav />
-      </div>
-      <section>
-      <div className="relative">
+    <>
+      <div className="relative"> 
         <div className="star-bg absolute star-container">
           <Stars />
         </div>
-        <div className="pb-24 font-semibold text-white flex flex-col items-center justify-center h-screen text-center relative z-10">
+        <div className="font-semibold text-white flex flex-col items-center justify-center h-screen text-center relative z-10">
           <h1 className="text-4xl md:text-5xl lg:text-6xl lg:pb-10 pb-7">
             Hi, I'm Dylan.
             <br />
@@ -26,7 +22,17 @@ export default function Home() {
           </span>
         </div>
       </div>
-      </section>
-    </div>
+
+      <div className="absolute top-0 w-full z-50">
+        <Nav />
+      </div>
+
+      <div className="relative bg-black">
+        <div>
+          <About/>
+          <h1 className="text-white">About</h1>
+        </div>
+      </div>
+    </>  
   );
 }
