70e499e5a96f40245a5df6da19328b4d7b61e873
DylanWDev <xpoweroutage@gmail.com>
2024-02-06 23:32:07 -0500

    i forgot what i did

--- src/app/components/Card/Card.jsx
+++ src/app/components/Card/Card.jsx
@@ -1,22 +1,27 @@
 import React from "react";
 
-export default function Card({ imageUrl, title, description, buttonText, buttonLink }) {
+export default function Card({
+  imageUrl,
+  title,
+  description,
+  buttonText,
+  buttonLink,
+}) {
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-xl w-80 backdrop-blur-sm bg-stone-800/40">
-      <div className="aspect-w-16 aspect-h-9"> {/* Aspect ratio container */}
-        <img className="object-cover w-full p-5 h-2/6 rounded-lg" src={imageUrl} alt="filler" />
-      </div>
-      <div className="px-6 py-4 h-60">
-        <div className="font-bold text-xl mb-2 text-white text-2xl">
-          {title}
+    <div className="w-96 h-52 bg-white drop-shadow-lg">
+      <div className="flex flex-row w-full bg-slate-600 h-5">
+        <div className="flex gap-1 items-center ml-2">
+          <div className="size-3 rounded-full bg-red-500"></div>
+          <div className="size-3 rounded-full bg-yellow-500"></div>
+          <div className="size-3 rounded-full bg-green-500"></div>
         </div>
-        <p className="text-gray-700 text-base text-md">{description}</p>
-        <span></span>
       </div>
-      <div className="flex justify-center mb-5">
-        <button className="hire-me rounded-lg transition hover:bg-violet-600 text-white font-semibold py-2 px-4 border border-gray-400 shadow">
-          <a href={buttonLink}>{buttonText}</a>
-        </button>
+      <div className="bg-blue-100 h-full">
+        <img
+          className="w-full h-full object-cover"
+          src="/images/project images/alarm-clock.png"
+          alt="Alarm Clock"
+        />
       </div>
     </div>
   );
